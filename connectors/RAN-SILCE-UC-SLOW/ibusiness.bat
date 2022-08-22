dc648064e43fe41d24e5fa45666f2cd480956542
Ashokkumar T.A <ashokkumar.ta@gmail.com>
2022-08-22 22:53:53 +0530

    Updated configuration for ISP & iBusiness connectors

---
+++ connectors/RAN-SILCE-UC-SLOW/ibusiness.bat
@@ -0,0 +1,4 @@
+docker-compose -f ibusiness.yaml stop
+docker-compose -f ibusiness.yaml rm -f
+#docker-compose -f ibusiness.yaml pull   
+docker-compose -f ibusiness.yaml up
--- connectors/RAN-SILCE-UC-SLOW/regu.sh
+++
@@ -1,4 +0,0 @@
-docker-compose -f ran-regulator.yaml stop
-docker-compose -f ran-regulator.yaml rm -f
-#docker-compose -f ran-regulator.yaml pull   
-docker-compose -f ran-regulator.yaml up
