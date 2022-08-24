8978131d8132c08aeb11e8eaf8e6680312000df4
Ashokkumar T.A <ashokkumar.ta@gmail.com>
2022-08-24 17:23:30 +0530

    Updated configuration

--- connectors/RAN-SILCE-UC-SLOW/mno2.bat
+++
@@ -1,4 +0,0 @@
-docker-compose -f mno-2.yaml stop
-docker-compose -f mno-2.yaml rm -f
-#docker-compose -f mno-2.yaml pull   
-docker-compose -f mno-2.yaml up
---
+++ connectors/RAN-SILCE-UC-SLOW/personx.sh
@@ -0,0 +1,4 @@
+docker-compose -f personx.yaml stop
+docker-compose -f personx.yaml rm -f
+#docker-compose -f personx.yaml pull   
+docker-compose -f personx.yaml up
