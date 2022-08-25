0a922ea6ae2ad66e16d3fbb70fd05b8e809feee6
Ashokkumar T.A <ashokkumar.ta@gmail.com>
2022-08-25 17:54:53 +0000

    update config

--- connectors/TMF-RAN-SILC-UC/ibusiness.sh
+++ connectors/TMF-RAN-SILC-UC/ibusiness.sh
@@ -1,4 +1,4 @@
-#docker compose -f ibusiness.yaml stop
-#docker compose -f ibusiness.yaml rm -f
+#sudo docker compose -f ibusiness.yaml stop
+#sudo ocker compose -f ibusiness.yaml rm -f
 #docker compose -f ibusiness.yaml pull   
 sudo docker compose -f ibusiness.yaml up
--- connectors/TMF-RAN-SILC-UC/isp.sh
+++ connectors/TMF-RAN-SILC-UC/isp.sh
@@ -1,4 +1,4 @@
-#docker compose -f isp.yaml stop
-#docker compose -f isp.yaml rm -f
+#sudo docker compose -f isp.yaml stop
+#sudo docker compose -f isp.yaml rm -f
 #docker compose -f isp.yaml pull   
 sudo docker compose -f isp.yaml up
