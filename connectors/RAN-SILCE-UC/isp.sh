fb596c4afe1720f5b30e06a7e8bdc51ed8f151bf
Ashok Kumar T. A <AshokKumar_TA@ad.infosys.com>
2022-08-25 17:39:08 +0530

    Config update

--- connectors/RAN-SILCE-UC/isp.sh
+++ connectors/RAN-SILCE-UC/isp.sh
@@ -1,4 +1,4 @@
-#docker-compose -f isp.yaml stop
-#docker-compose -f isp.yaml rm -f
-#docker-compose -f isp.yaml pull   
-sudo docker-compose -f isp.yaml up
+#docker compose -f isp.yaml stop
+#docker compose -f isp.yaml rm -f
+#docker compose -f isp.yaml pull   
+sudo docker compose -f isp.yaml up
