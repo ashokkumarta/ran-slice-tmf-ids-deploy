0879d65b44aa5102837910b8d7eed12f3ac821b4
Ashok Kumar T. A <AshokKumar_TA@ad.infosys.com>
2022-08-25 17:36:02 +0530

    Config update

--- connectors/TMF-RAN-SILC-UC/personx.sh
+++ connectors/TMF-RAN-SILC-UC/personx.sh
@@ -1,4 +1,4 @@
-docker compose -f personx.yaml stop
-docker compose -f personx.yaml rm -f
-#docker compose -f personx.yaml pull   
-docker compose -f personx.yaml up
+#docker-compose -f personx.yaml stop
+#docker-compose -f personx.yaml rm -f
+#docker-compose -f personx.yaml pull   
+sudo docker-compose -f personx.yaml up
