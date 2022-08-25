729015cd8d7344a6d3eae10d5988e8592daa0c4a
Ashok Kumar T. A <AshokKumar_TA@ad.infosys.com>
2022-08-25 23:38:27 +0530

    Config update

--- connectors/TMF-RAN-SILC-UC/ibusiness.sh
+++ connectors/TMF-RAN-SILC-UC/ibusiness.sh
@@ -1,4 +1,4 @@
-#sudo docker compose -f ibusiness.yaml stop
-#sudo ocker compose -f ibusiness.yaml rm -f
+sudo docker compose -f ibusiness.yaml stop
+sudo ocker compose -f ibusiness.yaml rm -f
 #docker compose -f ibusiness.yaml pull   
 sudo docker compose -f ibusiness.yaml up
