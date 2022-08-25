57eaa6f5e70e34189cf31063e17fe3d3344f18ab
Ashok Kumar T. A <AshokKumar_TA@ad.infosys.com>
2022-08-25 17:42:18 +0530

    Config update

--- connectors/TMF-RAN-SILC-UC/stop.sh
+++ connectors/TMF-RAN-SILC-UC/stop.sh
@@ -1,7 +1,7 @@
 sudo docker compose -f ibusiness.yaml stop
 sudo docker compose -f ibusiness.yaml rm -f
 sudo docker compose -f zbusiness.yaml stop
-sudo docker compose -f sbusiness.yaml rm -f
+sudo docker compose -f zbusiness.yaml rm -f
 sudo docker compose -f personx.yaml stop
 sudo docker compose -f personx.yaml rm -f
 sudo docker compose -f isp.yaml stop
